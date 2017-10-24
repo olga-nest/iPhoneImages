a91e7e3d14e481808ffbb4275f638fa09141d50c
olga-nest <olga.w.nesterova@gmail.com>
2017-10-23 17:35:19 -0700

    Initial Commit

---
+++ iPhoneImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iPhoneImages
+//
+//  Created by Olga on 10/23/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iPhoneImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneImages
+//
+//  Created by Olga on 10/23/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
